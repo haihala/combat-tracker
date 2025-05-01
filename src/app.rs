df425c2dd580aad58f16452825eefaa9ab5cd31e
Eero Häihälä <eero.haihala@gmail.com>
2025-05-01 15:45:21 +0300

    Adding, renaming and deleting creatures.

--- src/app.rs
+++ src/app.rs
@@ -1,32 +1,48 @@
 use std::io;
 
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyCode};
 use ratatui::{
     buffer::Buffer,
     layout::Rect,
     prelude::*,
-    style::{Style, Stylize},
+    style::Stylize,
     symbols::border,
-    text::{Line, Text},
+    text::Line,
     widgets::{Block, Borders, List, ListItem, ListState, Paragraph, Widget},
-    DefaultTerminal, Frame,
+    DefaultTerminal,
 };
 
 #[derive(Debug, Clone)]
 enum Mode {
     Normal,
+    Rename(String),
+    DamageCreature,
+    HealCreature,
+    EditNotes,
 }
 impl Mode {
     fn get_instructions(&self) -> Line {
         match self {
             Mode::Normal => Line::from(vec![
-                " Decrement ".into(),
-                "<Left>".blue().bold(),
-                " Increment ".into(),
-                "<Right>".blue().bold(),
-                " Quit ".into(),
-                "<Q> ".blue().bold(),
+                " Move: ".into(),
+                "hjkl".blue().bold(),
+                ", Exit: ".into(),
+                "Esc".blue().bold(),
+                ", Actions: (".into(),
+                "A".blue().bold(),
+                ")dd, (".into(),
+                "R".blue().bold(),
+                ")ename, (".into(),
+                "D".blue().bold(),
+                ")estroy ".into(),
             ]),
+            Mode::Rename(_) => Line::from(vec![
+                " Confirm: ".into(),
+                "Enter".blue().bold(),
+                ", Cancel: ".into(),
+                "Esc".blue().bold(),
+            ]),
+            _ => Line::from(vec![]),
         }
     }
 }
@@ -89,9 +105,17 @@ impl App {
             return Ok(());
         };
 
-        match self.mode {
+        let hovered_creature = self
+            .list_state
+            .selected()
+            .and_then(|index| self.creatures.get_mut(index));
+
+        match &self.mode {
             Mode::Normal => match ev.code {
+                // Quitting
                 KeyCode::Esc => self.running = false,
+
+                // Navigation
                 KeyCode::Char('h') => self.list_state.select_first(),
                 KeyCode::Char('j') => self.list_state.select(Some(
                     (self
@@ -110,8 +134,63 @@ impl App {
                     }
                 })),
                 KeyCode::Char('l') => self.list_state.select(Some(self.creatures.len() - 1)),
+
+                // Actions
+                KeyCode::Char('a') => {
+                    self.creatures.push(Creature {
+                        name: "".into(),
+                        ..Creature::default()
+                    });
+                    self.list_state.select(Some(self.creatures.len() - 1));
+                    self.mode = Mode::Rename(String::new());
+                }
+                KeyCode::Char('r') => {
+                    if let Some(creat) = hovered_creature {
+                        self.mode = Mode::Rename(creat.name.clone());
+                    }
+                }
+                KeyCode::Char('d') => {
+                    if hovered_creature.is_some() {
+                        let index = self.list_state.selected().unwrap();
+                        self.creatures.remove(index);
+                        if self.creatures.is_empty() {
+                            self.list_state.select(None);
+                        } else if self.creatures.len() == index {
+                            // Deleted final element in a non-empty list
+                            self.list_state.select(Some(self.creatures.len() - 1));
+                        }
+                    }
+                }
                 _ => {}
             },
+
+            Mode::Rename(old_name) => {
+                let selected_creature = hovered_creature.unwrap();
+                match ev.code {
+                    KeyCode::Enter => {
+                        self.mode = Mode::Normal;
+                    }
+                    KeyCode::Esc => {
+                        // Revert name
+                        selected_creature.name = old_name.clone();
+                        self.mode = Mode::Normal;
+                    }
+                    KeyCode::Backspace => {
+                        selected_creature.name = selected_creature
+                            .name
+                            .chars()
+                            .take(selected_creature.name.len() - 1)
+                            .collect();
+                    }
+                    KeyCode::Char(ch) => {
+                        selected_creature.name.push(ch);
+                    }
+
+                    _ => {}
+                }
+            }
+
+            _ => {}
         }
 
         Ok(())
@@ -123,7 +202,7 @@ impl Widget for App {
         let layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints(vec![
-                Constraint::Max(((self.creatures.len()) * 2 + 2) as u16),
+                Constraint::Max((self.creatures.len() + 2) as u16),
                 Constraint::Fill(1),
             ])
             .spacing(1)
@@ -140,14 +219,7 @@ impl Widget for App {
             .creatures
             .iter()
             .enumerate()
-            .map(|(i, creature)| {
-                let color = if selected_index == Some(i) {
-                    Color::Green
-                } else {
-                    Color::White
-                };
-                ListItem::from(creature.name.clone()).fg(color)
-            })
+            .map(|(i, creature)| creature.render(i, selected_index))
             .collect();
         let list = List::new(items).block(list_block);
         StatefulWidget::render(list, layout[0], buf, &mut self.list_state);
@@ -176,4 +248,37 @@ struct Creature {
     damage: usize,
     health: Option<usize>,
     notes: String,
+}
+
+impl Creature {
+    fn render(&self, index: usize, selected_index: Option<usize>) -> ListItem {
+        let selected = selected_index == Some(index);
+
+        // Inverse colors when selected
+        let (fg_color, bg_color) = if selected {
+            (Color::Black, Color::White)
+        } else {
+            (Color::White, Color::Black)
+        };
+
+        let name = if self.name.is_empty() {
+            "<empty>".into()
+        } else {
+            self.name.clone()
+        };
+
+        ListItem::from(name).fg(fg_color).bg(bg_color)
+    }
+}
+
+impl Default for Creature {
+    fn default() -> Self {
+        Creature {
+            name: "".into(),
+            quantity: 1,
+            damage: 0,
+            health: None,
+            notes: "".into(),
+        }
+    }
 }
