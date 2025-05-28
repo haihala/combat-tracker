b81960cf424682c491507ad0d62330057b9f0f4d
Eero Häihälä <eero.haihala@gmail.com>
2025-05-28 18:09:30 +0300

    Make logging conditional

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
+use clap::Parser;
 use simplelog::{Config, LevelFilter, WriteLogger};
 use std::{fs::File, io};
 
 mod app;
+mod args;
 
 fn main() -> io::Result<()> {
-    let _ = WriteLogger::init(
-        LevelFilter::Info,
-        Config::default(),
-        File::create("info.log").unwrap(),
-    );
+    let parsed_args = args::Args::parse();
+
+    if parsed_args.logging {
+        let _ = WriteLogger::init(
+            LevelFilter::Info,
+            Config::default(),
+            File::create("info.log").unwrap(),
+        );
+    }
+
     let terminal = ratatui::init();
     let result = app::App::new().run(terminal);
     ratatui::restore();
